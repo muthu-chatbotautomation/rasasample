c4f4bbf1d825f306184ca983141232ca2258066e
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-05 15:11:11 +0530

    Update killrasaprocess.sh

--- killrasaprocess.sh
+++ killrasaprocess.sh
@@ -7,4 +7,8 @@ for i in $arr
 do
       kill $i
 done
-
+conda init bash
+sudo chmod a+rwx models
+rasa train
+BUILD_ID=dontKillMe nohup rasa run --enable-api --cors "*" --debug &
+BUILD_ID=dontKillMe nohup rasa run actions --debug &
