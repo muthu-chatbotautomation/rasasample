8e04b94db7df2cc9df88da5ef798deb3b8764f7f
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-06 14:15:07 +0530

    Update Unit_test.py

--- Selenium_Webdriver/Unit_test.py
+++ Selenium_Webdriver/Unit_test.py
@@ -1,5 +1,5 @@
 import os
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.options import Options
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,11 +9,11 @@ dir = "/var/lib/jenkins/workspace/rasa pipeline/Selenium_Webdriver"
 chrome_driver_path = dir + "/chromedriver"
 
 # create a new Chrome session
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')  # Last I checked this was necessary.
 
-driver = webdriver.Chrome(chrome_driver_path,options=options)
+driver = webdriver.Chrome(chrome_driver_path)
 # driver = webdriver.Chrome(chrome_driver_path)
 driver.implicitly_wait(30)
 driver.maximize_window()
