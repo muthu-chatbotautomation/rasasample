8d7675e4b16fb5b9c3cee47f1212dab9e8579137
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-06 12:30:04 +0530

    Add files via upload

---
+++ Selenium_Webdriver/Unit_test.py
@@ -0,0 +1,24 @@
+import os
+from selenium.webdriver.chrome.options import Options
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# get the path of ChromeDriverServer
+#dir = "C:\\Users\\Admin\\PycharmProjects\\rasabot1"
+chrome_driver_path = dir + "\\chromedriver.exe"
+
+# create a new Chrome session
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+
+driver = webdriver.Chrome(chrome_driver_path,options=options)
+# driver = webdriver.Chrome(chrome_driver_path)
+driver.implicitly_wait(30)
+driver.maximize_window()
+
+# Navigate to the application home page
+driver.get("https://www.google.com/")
+print(driver.current_url)
+driver.quit()
