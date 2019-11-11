4a0e51f25a8c788ec5f4be0ac183b16049f09b75
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-11 10:17:04 +0530

    Update Unit_test.py

--- Selenium_Webdriver/Unit_test.py
+++ Selenium_Webdriver/Unit_test.py
@@ -3,23 +3,48 @@ from selenium.webdriver.chrome.options import Options
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 # get the path of ChromeDriverServer
+dir = "C:\\Users\\Admin\\PycharmProjects\\rasabot1"
+chrome_driver_path = dir + "\\chromedriver.exe"
 
-chrome_driver_path ="/var/lib/jenkins/workspace/rasa pipeline/Selenium_Webdriver/chromedriver"
 
-# create a new Chrome session
-options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+class Selenium_Test(unittest.TestCase):
+    def test_Selenium_Unit_Test(self):
+        # create a new Chrome session
+        options = Options()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')  # Last I checked this was necessary.
 
-driver = webdriver.Chrome(chrome_driver_path,options=options)
-# driver = webdriver.Chrome(chrome_driver_path)
-driver.implicitly_wait(30)
-driver.maximize_window()
+        driver = webdriver.Chrome(chrome_driver_path, options=options)
+        # driver = webdriver.Chrome(chrome_driver_path)
+        driver.implicitly_wait(30)
+        driver.maximize_window()
 
-# Navigate to the application home page
-driver.get("https://sidwebpage.s3.us-east-2.amazonaws.com/website/index.html")
-print(driver.current_url)
+        # chatbot testing simulation
+        time.sleep(12)
+        driver.get('http://sidwebpage.s3.us-east-2.amazonaws.com/website/index.html')
+        print(driver.current_url)
+        time.sleep(3)
+        driver.refresh()
+        time.sleep(3)
+        driver.find_element_by_xpath('//*[@id="webchat"]/div/button').click()
+        Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/input')
+        Mousepointer.send_keys("Hi")
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/button/img').click()
+        time.sleep(3)
+        # displaying and validating value
+        ExpectedReply = "Hey! How are you Easwar?"
+        Reply = driver.find_element_by_xpath('//*[@id="messages"]/div[2]/div/div/div/p/span').text
+        print(Reply)
+        self.assertEqual(Reply, ExpectedReply)
+        print("Success")
+
+
+if __name__ == '__main__':
+    unittest.main()
 
