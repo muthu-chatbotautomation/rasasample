42155390a820c598803454095961fc0431bb257a
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-11 12:18:35 +0530

    Update Unit_test.py

--- Selenium_Webdriver/Unit_test.py
+++ Selenium_Webdriver/Unit_test.py
@@ -12,11 +12,15 @@ class Selenium_Test(unittest.TestCase):
         options = Options()
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
+        options.add_argument("--incognito")
         options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+
         driver = webdriver.Chrome(chrome_driver_path, options=options)
         # driver = webdriver.Chrome(chrome_driver_path)
         driver.implicitly_wait(30)
         driver.maximize_window()
+        
+
         # chatbot testing simulation
         time.sleep(12)
         driver.get('http://sidwebpage.s3.us-east-2.amazonaws.com/website/index.html')
@@ -25,13 +29,13 @@ class Selenium_Test(unittest.TestCase):
         driver.refresh()
         time.sleep(3)
         driver.find_element_by_xpath('//*[@id="webchat"]/div/button').click()
-        Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/input').send_keys('hi')
+        Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/input')
         print("Mousepointer value{0}".format(Mousepointer))
+        Mousepointer.send_keys("Hi")
         time.sleep(2)
-        Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/button/img')
-        Mousepointer.click()
+        driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/button/img').click()
         time.sleep(5)
-         # displaying and validating value
+        # displaying and validating value
         ExpectedReply = "Hey! How are you Easwar?"
         Reply = driver.find_element_by_xpath('//*[@id="messages"]/div[2]/div/div/div/p/span').text
         print(Reply)
