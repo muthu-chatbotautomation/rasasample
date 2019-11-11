a1665b0727f10db89b98cb5022351d9e7fe24a83
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-11 13:20:06 +0530

    Update Unit_test.py

--- Selenium_Webdriver/Unit_test.py
+++ Selenium_Webdriver/Unit_test.py
@@ -15,9 +15,9 @@ class Selenium_Test(unittest.TestCase):
         # create a new Chrome session
         options = Options()
         options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
-        options.add_argument("--incognito")
-        options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--start-maximized");
+        options.addArguments("--headless");  # Last I checked this was necessary.
 
         driver = webdriver.Chrome(chrome_driver_path, options=options)
         # driver = webdriver.Chrome(chrome_driver_path)
