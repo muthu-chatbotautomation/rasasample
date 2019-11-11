78fc3beb0450819696a0dce2d8a72d4aa772514d
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-11 11:09:03 +0530

    Update Unit_test.py

--- Selenium_Webdriver/Unit_test.py
+++ Selenium_Webdriver/Unit_test.py
@@ -10,7 +10,6 @@ import unittest
 
 chrome_driver_path = "/var/lib/jenkins/workspace/rasa pipeline/Selenium_Webdriver/chromedriver"
 
-
 class Selenium_Test(unittest.TestCase):
     def test_Selenium_Unit_Test(self):
         # create a new Chrome session
@@ -18,12 +17,10 @@ class Selenium_Test(unittest.TestCase):
         options.add_argument('--no-sandbox')
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')  # Last I checked this was necessary.
-
         driver = webdriver.Chrome(chrome_driver_path, options=options)
         # driver = webdriver.Chrome(chrome_driver_path)
         driver.implicitly_wait(30)
         driver.maximize_window()
-
         # chatbot testing simulation
         time.sleep(12)
         driver.get('http://sidwebpage.s3.us-east-2.amazonaws.com/website/index.html')
@@ -36,18 +33,17 @@ class Selenium_Test(unittest.TestCase):
         print("Mousepointer value{0}".format(Mousepointer))
         Mousepointer.click()
         Mousepointer.send_keys("Hi")
-#         time.sleep(2)
-#         Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/button/img')
-#         Mousepointer.send_keys(u'\ue007')
-#         time.sleep(3)
-#         # displaying and validating value
-#         ExpectedReply = "Hey! How are you Easwar?"
-#         Reply = driver.find_element_by_xpath('//*[@id="messages"]/div[2]/div/div/div/p/span').text
-#         print(Reply)
-#         self.assertEqual(Reply, ExpectedReply)
-#         print("Success")
-
-
+        time.sleep(2)
+        Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/button/img')
+        Mousepointer.click()
+        time.sleep(3)
+         # displaying and validating value
+        ExpectedReply = "Hey! How are you Easwar?"
+        Reply = driver.find_element_by_xpath('//*[@id="messages"]/div[2]/div/div/div/p/span').text
+        print(Reply)
+        self.assertEqual(Reply, ExpectedReply)
+        print("Success")
 if __name__ == '__main__':
     unittest.main()
+
 
