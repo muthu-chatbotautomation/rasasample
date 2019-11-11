f8654426aab1c018a44d9ecdc2a63c5d120981c5
muthu-chatbotautomation <56630892+muthu-chatbotautomation@users.noreply.github.com>
2019-11-11 13:10:22 +0530

    Update Unit_test.py

--- Selenium_Webdriver/Unit_test.py
+++ Selenium_Webdriver/Unit_test.py
@@ -43,7 +43,6 @@ class Selenium_Test(unittest.TestCase):
         Mousepointer = driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/input')
 #         print("Mousepointer value{0}".format(Mousepointer))
         Mousepointer.click()
-        Mousepointer.clear()
         Mousepointer.send_keys("Hi")
 #         time.sleep(2)
 #         driver.find_element_by_xpath('//*[@id="webchat"]/div/div/form/button/img').click()
